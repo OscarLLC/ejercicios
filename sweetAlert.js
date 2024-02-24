1693c067857548196bbc33f33699beb592c35ca6
OscarLLC <oscarlopez88991@gmail.com>
2024-02-24 18:37:20 -0500

    creation home

---
+++ sweetAlert.js
@@ -0,0 +1,8 @@
+function aviso1() {
+  Swal.fire({
+    title: "Ejecico #1!",
+    text: "Realiza un código que permita calcular el área de un triángulo.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
