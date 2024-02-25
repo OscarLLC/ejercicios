111ce4253485c5f41cc56880eac90b441271a9c3
OscarLLC <oscarlopez88991@gmail.com>
2024-02-25 11:55:02 -0500

    load text ejercices

--- sweetAlert.js
+++ sweetAlert.js
@@ -6,3 +6,75 @@ function aviso1() {
     imageHeight: 200,
   });
 }
+function aviso2() {
+  Swal.fire({
+    title: "Ejecico #2!",
+    text: "Realiza un código para introducir dos números por teclado y sumarlos.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso3() {
+  Swal.fire({
+    title: "Ejecico #3!",
+    text: "Realiza un código para introducir un número por teclado y visualizar el número elevado al cuadrado.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso4() {
+  Swal.fire({
+    title: "Ejecico #4!",
+    text: "Realiza un código que imprima por pantalla la suma de 1234 y 532.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso5() {
+  Swal.fire({
+    title: "Ejecico #5!",
+    text: "Realiza un código que imprima por pantalla la resta de 1234 y 532.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso6() {
+  Swal.fire({
+    title: "Ejecico #6!",
+    text: "Realiza un código que imprima por pantalla la multiplicación de 1234 y 532.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso7() {
+  Swal.fire({
+    title: "Ejecico #7!",
+    text: "Realiza un código que imprima por pantalla la división de 1234 entre 532..",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso8() {
+  Swal.fire({
+    title: "Ejecico #8!",
+    text: "Realiza un código que imprima por pantalla el módulo de 1234 entre 532.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso9() {
+  Swal.fire({
+    title: "Ejecico #9!",
+    text: "Realiza un código que convierta de euros a dólares. Recibirá un número decimal correspondiente a la cantidad en euros y contestará con la cantidad correspondiente en dólares.",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
+function aviso10() {
+  Swal.fire({
+    title: "Ejecico #10!",
+    text: "Realiza un código que calcule el área de un rectángulo del cual se le proporcionará por el teclado su altura y anchura (números decimales).",
+    imageWidth: 600,
+    imageHeight: 200,
+  });
+}
